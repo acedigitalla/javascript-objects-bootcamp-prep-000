fbd79fd97753023d909a81122b54d5344772d830
Alex Edwards <info@acedigitalagency.net>
2018-03-25 03:42:53 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,6 +9,6 @@ function updatePlaylist(playlist, artist, title) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
+  delete playlist.artist;
   return playlist;
 }
